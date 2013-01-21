1362ce74d028228dab4448f6d083e90e4782f204
Alex Russell <slightlyoff@chromium.org>
2013-01-21 10:45:54 +0000

    Correctly persist the extension's aggregate data across sessions. Rate-limit disk writes. Stub out server-sending of content.

--- extension/background.js
+++ extension/background.js
@@ -2,6 +2,42 @@
 // display the totals.
 
 var debug = false;
+var serverDebug = true; // false;
+
+var WRITE_RATE = 15000; // Only write to disk every 15 seconds or so, max.
+var REPORT_URL = (serverDebug ?
+                      "http://localhost:8080" :
+                      "https://meaningless-stats.appspot.com") + "/report";
+
+var storage = chrome.storage.local;
+
+var rateLimited = function(func, interval) {
+  var lastRun;
+  var timer;
+  return function() {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    var f = func.bind(this);
+    var runner = function() {
+      timer = null;
+      lastRun = Date.now();
+      f();
+    };
+    if (!lastRun || (Date.now() - lastRun >= interval)) {
+      // If we haven't run at least once in the allotted time-frame, call
+      // immediately.
+      runner();
+    } else {
+      // Else wait until the time period has elapsed and try again.
+      var i = interval;
+      if (lastRun) {
+        i = interval - (Date.now() - lastRun);
+      }
+      timer = setTimeout(runner, i);
+    }
+  };
+};
 
 var AggregateElementData = function() {
   ElementData.call(this);
@@ -13,13 +49,25 @@ AggregateElementData.prototype = Object.create(ElementData.prototype, {
     enumerable: false,
     writable: true,
     configruable: true,
-    value: function(data, sender) {
+    value: function(data, type) {
       this.total += data.total;
       this.tags.merge(data.tags);
       this.schemaDotOrgItems.merge(data.schemaDotOrgItems);
       this.microformatItems.merge(data.microformatItems);
       this.ariaItems.merge(data.ariaItems);
       this.semantics.merge(data.semantics);
+      if (type == "pageload") {
+        this.documents++;
+      } else if (type == "update") {
+        this.updates++;
+      } else {
+        if (data.documents) {
+          this.documents += data.documents;
+        }
+        if (data.updates) {
+          this.updates += data.updates;
+        }
+      }
     }
   },
   summary: {
@@ -37,10 +85,79 @@ AggregateElementData.prototype = Object.create(ElementData.prototype, {
         semantics: this.semantics,
       };
     }
+  },
+});
+
+var PersistentAggregateElementData = function(keyname) {
+  AggregateElementData.call(this);
+
+  if (keyname) {
+    this.key = keyname;
+    this.rehydrate();
+  }
+};
+PersistentAggregateElementData.prototype =
+    Object.create(AggregateElementData.prototype, {
+  aggregate: {
+    enumerable: false, writable: true, configruable: true,
+    value: function() {
+      AggregateElementData.prototype.aggregate.apply(this, arguments);
+      this.persist();
+    }
+  },
+  persist: {
+    enumerable: false, writable: true, configruable: true,
+    value: rateLimited(function() {
+      if (!this.key) return;
+      debug && console.log("persiting", this.key, Date.now());
+      var data = {};
+      data[this.key] = this.summary;
+      storage.set(data);
+    }, WRITE_RATE)
+  },
+  rehydrate: {
+    enumerable: false, writable: true, configruable: true,
+    value: function() {
+      if (!this.key) return;
+      storage.get(this.key, function(data) {
+        if (data[this.key]) {
+          this.aggregate(data[this.key]);
+        }
+      }.bind(this));
+    }
+  },
+  clear: {
+    enumerable: false, writable: true, configruable: true,
+    value: function() {
+      if (!this.key) return;
+      storage.remove(this.key);
+    }
   }
 });
 
-var totals = new AggregateElementData();
+var totals = new PersistentAggregateElementData("totals");
+var delta = new PersistentAggregateElementData("delta");
+
+var sendToServer = function() {
+  return;
+  // Upload to our logging service and, on success, clear out the delta set.
+
+  // FIXME: rate limit and schedule!
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function(e) {
+    debug && console.log(e);
+    if (xhr.readyState == 4) {
+      delta.clear();
+    }
+  };
+  xhr.open("POST", REPORT_URL, true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send("data=" + encodeURIComponent(JSON.stringify({
+    showReport: false,
+    delta: delta,
+    totals: totals
+  })));
+};
 
 chrome.extension.onMessage.addListener(
   function(msg, sender) {
@@ -72,14 +189,8 @@ chrome.extension.onMessage.addListener(
       });
     }
 
-    totals.aggregate(data, sender);
-    if (msg.type == "pageload") {
-      totals.documents++;
-    } else {
-      totals.updates++;
-    }
-
-    broadcast(totals.summary);
+    totals.aggregate(data, msg.type);
+    sendToServer();
 });
 
 var ports = [];
@@ -88,7 +199,7 @@ chrome.extension.onConnect.addListener(function(port) {
   port.broadcast = true;
   ports.push(port);
   port.onDisconnect.addListener(function() {
-    console.log("disconnected!");
+    debug && console.log("disconnected!");
     var pi = ports.indexOf(port);
     if (pi >= 0) {
       // Remove from the list to broadcast to.
@@ -107,6 +218,7 @@ chrome.extension.onConnect.addListener(function(port) {
   });
 });
 
+/*
 var broadcast = function(data) {
   ports.forEach(function(p) {
     if(p.broadcast) {
@@ -114,3 +226,4 @@ var broadcast = function(data) {
     }
   });
 };
+*/
